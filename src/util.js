5ecf2a991cece7ac5a12e5503fb2722df2d6775e
Valérian Galliat <val@codejam.info>
2015-03-21 21:51:17 +0100

    Refactor again, decouple Markdown parsing rendering
    * Add HTML minification.
    * Use a template file for 'index.md' generation instead of modifiying
      in-place.
    * Refactor the makefile and shell scripts to retrieve list of posts and
      generate index.
    * Keep partial HTML files (raw parsed Markdown).
    * Include page title in partial HTML for easy reuse.
    * Update layout and base detection.

--- src/util.js
+++ src/util.js
@@ -3,10 +3,10 @@ export function layout (file) {
 
   switch (true) {
     case /^\d{4}$/.test(parts[1]): return 'post'
-    case parts[1] === 'index.md': return 'index'
+    case parts[1].split('.')[0] === 'index': return 'index'
     default: return 'page'
   }
 }
 
 export const base = file =>
-  file.split('/').map(() => '..').slice(2).join('/')
+  file.split('/').map(() => '..').slice(2).join('/') || '.'
